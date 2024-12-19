c929087ec5e85c0b193735048d19dc21787b8b05
Ramon G. Gonze <ramongonze@gmail.com>
2024-12-19 19:39:47 -0300

    Optimized re-identification by using np.unique with parameter return_index instead of return_counts and optimized attribute infrence by using also return_index of np.unique but also implementing a for loop over all sensitive values to get the most frequent element of each partition instead of using numpy methods to get them.

--- privattacks/attacks.py
+++ privattacks/attacks.py
@@ -1,5 +1,10 @@
+import csv
 import privattacks
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import multiprocessing
+import itertools as it
 from typing import Union, List
 
 class Attack():
@@ -16,7 +21,7 @@ class Attack():
         Attributes:
             - data (privattacks.Data): Stores the dataset object, providing access to 
             the dataset's attributes, such as columns (`cols`) and number of rows 
-            (`num_rows`).
+            (`n_rows`).
         """
         self.data = data
 
@@ -44,6 +49,10 @@ class Attack():
                 raise ValueError(f"The sensitive attribute '{att}' is not in the dataset.")
         return sensitive
                 
+    def _partial_result_reid(self, qids_subset):
+        """Required by multiprocessing package in order to use imap_unordered()."""
+        return (qids_subset, self.posterior_reid(qids_subset))
+
     def prior_reid(self):
         """
         Prior vulnerability of probabilistic re-identification attack.
@@ -51,7 +60,7 @@ class Attack():
         Returns:
             float: Prior vulnerability.
         """
-        return 1 / self.data.num_rows
+        return 1 / self.data.n_rows
 
     def prior_ai(self, sensitive:Union[str, List[str]]):
         """
@@ -71,7 +80,7 @@ class Attack():
         for att in sensitive:
             sensitive_idx = self.data.col2int(att)
             _, counts = np.unique(self.data.dataset[:, sensitive_idx], return_counts=True)
-            posteriors[att] = int(counts.max()) / self.data.num_rows
+            posteriors[att] = int(counts.max()) / self.data.n_rows
         return posteriors
 
     def posterior_reid(self, qids:list[str]):
@@ -88,10 +97,10 @@ class Attack():
         qids_idx = [self.data.col2int(att) for att in qids]
         
         # Groupby by qids
-        _, counts = np.unique(self.data.dataset[:, qids_idx], axis=0, return_counts=True)
-        posterior = len(counts)/self.data.num_rows
+        _, partition_starts = np.unique(self.data.dataset[:, qids_idx], axis=0, return_index=True)
+        n_partitions = len(partition_starts)
         
-        return posterior
+        return n_partitions/self.data.n_rows
     
     def posterior_ai(self, qids:list[str], sensitive:Union[str, List[str]]):
         """
@@ -114,32 +123,43 @@ class Attack():
         qids_idx = [self.data.col2int(att) for att in qids] # Qid column indices
         qid_values = self.data.dataset[:, qids_idx] # Partition identifiers
         
+        # Find unique qid_values, partition starts (indexes) and partition counts
+        _, partition_starts = np.unique(qid_values, axis=0, return_index=True)
+        n_partitions = len(partition_starts)
+
+        # Attribute inference
         posteriors = {}
         for sens in sensitive:
             sensitive_idx = self.data.col2int(sens) # Sensitive column index
             sensitive_values = self.data.dataset[:, sensitive_idx] # Sensitive attribute columns
 
-            # Find unique qid_values, partition starts (indexes) and partition counts
-            _, partition_starts, partition_sizes = np.unique(qid_values, axis=0, return_index=True, return_counts=True)
-            
-            # Calculate the adversary's success for each partition
-            posterior = 0
-            for i, start_idx in enumerate(partition_starts):
-                if i == len(partition_starts)-1:
-                    end_idx = len(sensitive_values)
-                else:
-                    end_idx = partition_starts[i+1]
+            cur_value = -2
+            next_partition, cur_count, max_count, posterior = 0, 0, 0, 0
+            # Go through all partitions and find the most frequent element
+            for i in np.arange(self.data.n_rows):
+                # Check if the current partition has finished
+                new_partition = (i == partition_starts[next_partition])
 
-                partition_size = partition_sizes[i]
-                # Find the most frequent element in the partition
-                _, counts = np.unique(sensitive_values[start_idx:end_idx], return_counts=True)
+                cur_count += 1
+                max_count = max(max_count, cur_count)
+
+                # If we've started a new partition or reach the end of the last partition
+                if new_partition or i == self.data.n_rows-1:
+                    posterior += max_count
+
+                    # Reset pointers
+                    next_partition = min(next_partition+1, n_partitions-1)
+                    cur_value = sensitive_values[i]
+                    max_count, cur_count = 0, 0
                 
-                # The adversary's success for the current partition is the most frequent element / partition size
-                posterior += counts.max()
-            
-            posteriors[sens] = posterior / self.data.num_rows
-        
-        return posteriors
+                # If the current value has changed or if we've started a new partition
+                if cur_value != sensitive_values[i] or new_partition:
+                    cur_value = sensitive_values[i]
+                    cur_count = 0
+                
+            posteriors[sens] = posterior/self.data.n_rows
+
+        return posteriors        
 
     def posterior_reid_ai(self, qids:list[str], sensitive:Union[str, List[str]]):
         """
@@ -163,32 +183,95 @@ class Attack():
         qid_values = self.data.dataset[:, qids_idx] # Partition identifiers
         
         # Find unique qid_values, partition starts (indexes) and partition counts
-        _, partition_starts, partition_sizes = np.unique(qid_values, axis=0, return_index=True, return_counts=True)
+        _, partition_starts = np.unique(qid_values, axis=0, return_index=True)
+        n_partitions = len(partition_starts)
 
         # Re-identification
-        posterior_reid = len(partition_sizes)/self.data.num_rows
+        posterior_reid = n_partitions/self.data.n_rows
 
-        # Attribute inference
+        # Attribute Inference
         posteriors_ai = {}
         for sens in sensitive:
             sensitive_idx = self.data.col2int(sens) # Sensitive column index
             sensitive_values = self.data.dataset[:, sensitive_idx] # Sensitive attribute columns
 
-            # Calculate the adversary's success for each partition
-            posterior = 0
-            for i, start_idx in enumerate(partition_starts):
-                if i == len(partition_starts)-1:
-                    end_idx = len(sensitive_values)
-                else:
-                    end_idx = partition_starts[i+1]
+            cur_value = -2
+            next_partition, cur_count, max_count, posterior = 0, 0, 0, 0
+            # Go through all partitions and find the most frequent element
+            for i in np.arange(self.data.n_rows):
+                # Check if the current partition has finished
+                new_partition = (i == partition_starts[next_partition])
 
-                partition_size = partition_sizes[i]
-                # Find the most frequent element in the partition
-                _, counts = np.unique(sensitive_values[start_idx:end_idx], return_counts=True)
+                cur_count += 1
+                max_count = max(max_count, cur_count)
+
+                # If we've started a new partition or reach the end of the last partition
+                if new_partition or i == self.data.n_rows-1:
+                    posterior += max_count
+
+                    # Reset pointers
+                    next_partition = min(next_partition+1, n_partitions-1)
+                    cur_value = sensitive_values[i]
+                    max_count, cur_count = 0, 0
                 
-                # The adversary's success for the current partition is the most frequent element / partition size
-                posterior += counts.max()
-            
-            posteriors_ai[sens] = posterior / self.data.num_rows
+                # If the current value has changed or if we've started a new partition
+                if cur_value != sensitive_values[i] or new_partition:
+                    cur_value = sensitive_values[i]
+                    cur_count = 0
+                
+            posteriors_ai[sens] = posterior/self.data.n_rows
+
+        return posterior_reid, posteriors_ai
+
+    def posterior_reid_subset(self, qids:list[str], num_min, num_max, save_file=None, n_processes=1, verbose=False):
+        """Posterior vulnerability of probabilistic re-identification attack for subsets of qids.
         
-        return posterior_reid, posteriors_ai    
+        Parameters:
+            - qids (list[str]): List of quasi-identifiers.
+            - min_size (int): Minimum size of subset of qids.
+            - max_size (int): Maximum size of subset of qids.
+            - save_file (str, optional): File name to save the results. They will be saved in CSV format.
+            - n_processes (int, optional): Number of processes to run the method in parallel using multiprocessing package. Default is 1.
+            - verbose (bool, optinal): Show the progress. Default is False.
+
+        Returns:
+            - float: Posterior vulnerability.
+        """
+        posteriors = []
+        if save_file is not None:
+            # Create a new file with the header
+            posteriors.to_csv(save_file, index=False)
+        
+        with multiprocessing.Pool(processes=n_processes) as pool:
+            # For qid combinations in the given range run re-identification attack
+            for n_qids in tqdm(np.arange(num_min, num_max + 1), desc="Number of qids", disable=(not verbose)):
+                partial_result = []
+                
+                # Run the attack for all combination of 'n_qids' QIDs
+                results = pool.imap_unordered(
+                    self._partial_result_reid,
+                    it.combinations(qids, n_qids)
+                )
+
+                # Get results from the pool
+                for qids, posterior in results:
+                    partial_result.append([n_qids, ", ".join(qids), posterior])
+                
+                # Save once finished all combinations for 'n_qids'
+                posteriors.append()
+
+                # Append to save_file
+                with open(save_file, mode="a", newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerows(partial_result)
+
+                if save_file is not None:
+                    partial_result.to_csv(
+                        save_file,
+                        index=False,
+                        mode="a",     
+                        header=False,
+                        float_format="%.8f"
+                    )
+# pd.DataFrame(columns=["n_qids", "qids", "posterior_reid"])
+        return posteriors
